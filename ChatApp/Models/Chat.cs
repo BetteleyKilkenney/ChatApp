f1ea45b7eef7ed1e54cf3353ad52284369e9843f
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-10-28 20:24:13 -0400

    No Followers

---
+++ ChatApp/Models/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatApp.Models
+{
+    public class Chat
+    {
+        public int ChatID { get; set; }
+        public string ChatText { get; set; }
+        public DateTime ChatTimeStamp { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
